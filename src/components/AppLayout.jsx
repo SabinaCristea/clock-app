1b77d50baace99baf55a8a121e5d6978706722f9
SabinaCristea <sabinacristea997@gmail.com>
2024-04-16 20:10:48 +0300

    Modified image height

--- src/components/AppLayout.jsx
+++ src/components/AppLayout.jsx
@@ -9,9 +9,8 @@ function AppLayout() {
     <div className="relative ">
       <img
         src={`../assets/mobile/bg-image-${image}time.jpg`}
-        // src={`../assets/mobile/bg-image-nighttime.jpg`}
         alt="Daytime phone"
-        className="object-cover	absolute z-[-10] w-[100%] h-auto "
+        className="object-cover	absolute z-[-10] w-[100%] h-[100vh]"
       />
       <div className="absolute w-[100vw] h-[100vh] bg-black opacity-[0.4] z-[-1]"></div>
       <div className="px-[2.6rem] pt-[3.2rem]">
